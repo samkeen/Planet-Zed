a1cc422de69eb738ef06bbe864e986073d815a73
samkeen <samkeen@lululemon.com>
2020-07-04 16:20:34 -0700

    init cicuit in place

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        // dont destroy the component I'm attached to
+        DontDestroyOnLoad(gameObject);    
+    }
+
+    void Start()
+    {
+        Invoke("LoadScene", 2.0f);
+    }
+
+    // Update is called once per frame
+    void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
