d126f6298d5c2b8f6df27d1b9f7b530c58abe557
samkeen <samkeen@lululemon.com>
2020-07-04 22:11:16 -0700

    added player ship

---
+++ Assets/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("in ms^-1")] [SerializeField] private float speed = 12f;
+    [Tooltip("in m")] [SerializeField] private float xRange = 5f;
+    [Tooltip("in m")] [SerializeField] private float yRange = 2.6f;
+
+// Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+        // ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        // x, y, z (pitch, yaw, roll)
+        transform.localRotation = Quaternion.Euler(0f, 90f, 0f);
+        
+    }
+
+    private void ProcessTranslation()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * speed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+}
