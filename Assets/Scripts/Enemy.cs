f644a8e4646523c2829c753d4e418d44ac5abcce
samkeen <samkeen@lululemon.com>
2020-07-07 20:52:07 -0700

    starting enemy explosions

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject deathFX;
+    private void Start()
+    {
+        AddNonTriggerBoxCollimder();
+    }
+
+    private void AddNonTriggerBoxCollimder()
+    {
+        Collider collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        print("Collided with enemy: " + gameObject.name);
+        Instantiate(deathFX, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
